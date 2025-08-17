c7925de86fa6d7bafbed1b09fd7f63f6c4f0faff
udit012435 <udit012435@gmail.com>
2025-08-17 18:13:59 +0530

    update

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ import settingRouter from './routes/Setting.js'
 
 connectToDatabase()
 const app = express()
-app.use(cors())
+app.use(cors(
+    {
+        origin: "https://employees-iota.vercel.app",
+        credentials: true
+    }
+))
 app.use(express.json())
 app.use(express.static('public/uploads'))
 app.use('/api/auth', authRouter)
