35bf30db6dc81a85f96cf59f073e7ea6158ab305
udit012435 <udit012435@gmail.com>
2025-09-17 17:43:39 +0530

    init

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
 import cors from 'cors'
 import authRouter from './routes/auth.js'
 import departmentRouter from './routes/department.js'
