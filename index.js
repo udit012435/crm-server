fa7896c523f9b60845d7a204fcaf1a705592930b
udit012435 <udit012435@gmail.com>
2025-09-17 17:21:36 +0530

    db password changed

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use(cors(
     }
 ))
 app.use(express.json())
-app.use('/uploads',express.static('public/uploads'))
+// app.use('/uploads',express.static('public/uploads'))
+app.use(express.static('public/uploads'))
 app.use('/api/auth', authRouter)
 app.use('/api/department', departmentRouter)
 app.use('/api/employee' , employeeRouter)
