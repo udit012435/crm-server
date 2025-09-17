d14a59dca1635a55efe65ece962f4b45b537398a
udit012435 <udit012435@gmail.com>
2025-09-17 17:34:52 +0530

    error

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config() 
 import cors from 'cors'
 import authRouter from './routes/auth.js'
 import departmentRouter from './routes/department.js'
